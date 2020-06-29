3f92ec3eee2b179d83a93057031ba19fbb354538
aBruisedReed <snare909@gmail.com>
2020-06-29 21:25:52 +0900

    메인 애니메이션 완료

--- BurgerDelivery/src/view/MainController.java
+++ BurgerDelivery/src/view/MainController.java
@@ -3,8 +3,14 @@ package view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.WindowEvent;
+import javafx.util.Duration;
 import service.CommonService;
 import service.CommonServiceImpl;
 import service.LoginService;
@@ -15,6 +21,7 @@ public class MainController extends Controller implements Initializable {
 	private Parent root;
 	private LoginService loginSrv;
 	private CommonService comSrv;
+	private boolean flag;
 	
 	@Override
 	public void setRoot(Parent root) {
@@ -27,6 +34,28 @@ public class MainController extends Controller implements Initializable {
 		// TODO Auto-generated method stub
 		loginSrv = new LoginServiceImpl();
 		comSrv = new CommonServiceImpl();
+		flag=true;
+	}
+	
+	public void paneProc() {
+		if(flag) {
+			animMain();
+			flag=false;
+		}
+	}
+	
+	public void animMain() {
+		ImageView imageView1 = (ImageView) root.lookup("#imageView1");
+		
+		TranslateTransition tt = new TranslateTransition();
+		tt.setNode(imageView1);
+		tt.setDuration(new Duration(20000));
+		
+		tt.setFromX(0); 
+		tt.setToX(-640);
+		tt.setAutoReverse(true);
+		tt.setCycleCount(50);
+		tt.play();
 	}
 	
 	public void loginStart() {
